3b7c339385e37fd422a8d954a41d0cdc60f8cff6
snehakumaresan <snehakumares@gmail.com>
2021-04-07 15:31:54 +0530

    Day 4

--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,9 @@
 public class Student {
     private static int noofstudents=0;
     private String name;
+    public boolean lowIncomeFamily;
+    public boolean meritEligible;
+    public int age;
     public Student(String firstName,String lastName){
         this.name = firstName + " " + lastName;
         noofstudents+=1;
@@ -21,5 +24,15 @@ public class Student {
     public String greeting(){
         return "Hello "+this.name+"!";
     }
+    public boolean meritCumMeans(){
+        return (this.meritEligible && this.lowIncomeFamily) ;
+    }
 
+    public String eligibleToVote() {
+        if(this.age<18){
+            return "No";
+        }
+        else
+            return "Yes";
+    }
 }
--- src/StudentTest.java
+++ src/StudentTest.java
@@ -14,11 +14,52 @@ public class StudentTest {
         assertEquals("Hello Abc xyz!",s1.greeting());
     }
     @Test
-    public void testNumberStudents(){
+    public void testNumberStudents() {
         Student s1 = new Student();
         Student s2 = new Student();
         Student s3 = new Student();
-        assertEquals(3,Student.numberOfStudents());
+        assertEquals(3, Student.numberOfStudents());
     }
-
+    @Test
+    public void testCheckEligibility(){
+        Student s1 = new Student();
+        s1.meritEligible = true;
+        s1.lowIncomeFamily = false;
+        assertEquals(false,s1.meritCumMeans());
+    }
+    @Test
+    public void testVoteEligibility(){
+        Student s1 = new Student();
+        s1.age = 20;
+        assertEquals("Yes",s1.eligibleToVote());
+    }
+    @Test
+    public void testStudentsloop(){
+        int i;
+        Student s1;
+        for(i=0;i<2;i++){
+            s1 = new Student();
+        }
+        i=0;
+        while(i<2){
+          s1 = new Student();
+          i++;
+        }
+        i=0;
+        do{
+            s1=new Student();
+            i++;
+        }while(i<2);
+        assertEquals(6,Student.numberOfStudents());
+    }
+    @Test
+    public void testArray(){
+        Student[] s1 = new Student[4];
+        for(int i=0;i<4;i++){
+            s1[i] = new Student(Integer.toString(i),"");
+        }
+        for(Student s:s1){
+            System.out.println(s.greeting());
+        }
+    }
 }
